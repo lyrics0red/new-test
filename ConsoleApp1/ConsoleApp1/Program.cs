f38629671cea05fb0610cf371ec8eb93aea1c0f2
kkrasnoslobodtsev <90769620+lyrics0red@users.noreply.github.com>
2022-10-10 17:31:23 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    struct MyStruct
+    {
+        public int X;
+        public int Y
+        {
+            get => X;
+            set => X = value;
+        }
+
+        public MyStruct(int y, int x)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
